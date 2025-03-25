a3dcb4c2276000e61ab8d181dd2f6b979d3a0f9b
eunwoooo <tabcun88@naver.com>
2025-03-25 11:24:39 +0900

    Add files via upload

---
+++ 윤은우/03/js/conditional2.js
@@ -0,0 +1,11 @@
+let score = prompt("프로그래밍 점수: ");
+
+if (score !== null){
+  if(parseInt(score) >=90){
+    alert("A 학점");
+  } else if (parseInt(score) >= 80) {
+    alrer("B 학점");
+  } else {
+    alrert("C 학점");
+  }
+}
